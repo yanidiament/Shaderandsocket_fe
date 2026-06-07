cfd200e35c3b1331822e294072c23d46df667675
yanidiament <yanidiament@gmail.com>
2026-06-06 23:05:44 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Shaderandsocket_fe/', // 
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        viewer: resolve(__dirname, 'viewer.html'),
-      },
-    },
-  },
-});
+})
